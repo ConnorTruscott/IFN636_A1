91f9a1d5e28e5aeb6184ec34485349a14bd5f2ad
Connor <connortruscott@hotmail.com>
2025-08-14 22:54:46 +1000

    HACK-1 Epic 1: Team Management Complete with Tests

--- backend/test/test.js
+++ backend/test/test.js
@@ -2,7 +2,8 @@ const chai = require('chai');
 const sinon = require('sinon');
 const mongoose = require('mongoose');
 const Team = require('../models/Team');
-const {createTeam, addTeam} = require('../controllers/teamController');
+const {createTeam, addTeam, getTeams, updateTeam, deleteTeam} = require('../controllers/teamController');
+const { json } = require('express');
 const {expect} = chai;
 
 describe('Team Controller - createTeam', () =>{
@@ -49,5 +50,164 @@ describe('Team Controller - createTeam', () =>{
         expect(res.json.calledWithMatch({message: 'DB Error'})).to.be.true;
 
         createStub.restore();
+    });
+});
+
+describe('Team Controller - getTeams', () =>{
+    it('should return a team successfully', async() =>{
+        const teamId = new mongoose.Types.ObjectId();
+        
+        const teams = [
+            {_id: new mongoose.Types.ObjectId(), name: "Team 1", teamId},
+            {_id: new mongoose.Types.ObjectId(), name: "Team 2", teamId}
+        ];
+
+        const findStub = sinon.stub(Team, 'find').resolves(teams);
+
+        const req = {user: {id: teamId}};
+        const res = {
+            json: sinon.spy(),
+            status: sinon.stub().returnsThis()
+        };
+
+        await getTeams(req,res);
+
+        expect(findStub.calledOnceWith({teamId})).to.be.string;
+        expect(res.json.calledWith(teams)).to.be.true;
+        expect(res.status.called).to.be.false;
+
+        findStub.restore();
+    });
+
+    it('should return 500 if team not found', async() => {
+        const findStub = sinon.stub(Team, 'find').throws(new Error('DB Error'));
+        const req = {user:{id: new mongoose.Types.ObjectId()}};
+        const res = {status: sinon.stub().returnsThis(), json: sinon.spy()};
+
+        await getTeams(req,res);
+
+        expect(res.status.calledWith(500)).to.be.true;
+        expect(res.json.calledWithMatch({message: 'DB Error'})).to.be.true;
+
+        findStub.restore();
+    });
+});
+
+describe("Team Controller - updateTeam", () => {
+    it("should update team successfully", async()=>{
+        const teamId = new mongoose.Types.ObjectId();
+        const existingTeam={
+            _id: teamId,
+            name: "Old Name",
+            members: "Old Members",
+            skill: "Old Skill",
+            save: sinon.stub().resolvesThis(),
+        };
+
+        const findByIdStub = sinon.stub(Team, 'findById').resolves(existingTeam);
+
+        const req = {
+            params: {id: teamId},
+            body: {name:"New Name"}
+        };
+        const res = {
+            json: sinon.spy(),
+            status: sinon.stub().returnsThis()
+        };
+
+        await updateTeam(req,res);
+
+        expect(existingTeam.name).to.equal("New Name");
+        expect(res.status.called).to.be.false;
+        expect(res.json.calledOnce).to.be.true;
+
+        findByIdStub.restore()
+    });
+
+    it("should return 404 if no team is found", async() => {
+        const findByIdStub = sinon.stub(Team, 'findById').resolves(null);
+        const req = {params: {id: new mongoose.Types.ObjectId()}, body: {}};
+        const res = {
+            status: sinon.stub().returnsThis(),
+            json: sinon.spy()
+        };
+
+        await updateTeam(req,res);
+        expect(res.status.calledWith(404)).to.be.true;
+        expect(res.json.calledWith({message: "Team Not Found"})).to.be.true;
+
+        findByIdStub.restore();
+    });
+
+    it("should return 500 on error", async() => {
+        const findByIdStub = sinon.stub(Team, 'findById').throws(new Error('DB Error'));
+
+        const req = {params: {id: new mongoose.Types.ObjectId()}, body:{}};
+        const res = {
+            status: sinon.stub().returnsThis(),
+            json: sinon.spy()
+        };
+
+        await updateTeam(req,res);
+
+        expect(res.status.calledWith(500)).to.be.true;
+        expect(res.json.called).to.be.true;
+
+        findByIdStub.restore();
+    });
+});
+
+describe("Team Controller - deleteTeam", () =>{
+    it('should delete a team succesfully', async() => {
+        const req = {params: {id: new mongoose.Types.ObjectId().toString()}};
+
+        const team = {remove: sinon.stub().resolves()};
+        const findByIdStub = sinon.stub(Team, 'findById').resolves(team);
+
+        const res={
+            status:sinon.stub().returnsThis(),
+            json: sinon.spy()
+        };
+
+        await deleteTeam(req,res);
+
+        expect(findByIdStub.calledOnceWith(req.params.id)).to.be.true;
+        expect(team.remove.calledOnce).to.be.true;
+        expect(res.json.calledWith({message: 'Team deleted successfully'})).to.be.true;
+
+        findByIdStub.restore();
+    });
+
+    it("should return 404 if team is not found", async() =>{
+        const findByIdStub =sinon.stub(Team, 'findById').resolves(null);
+        const req = {params: {id: new mongoose.Types.ObjectId().toString()}};
+        const res = {
+            status: sinon.stub().returnsThis(),
+            json: sinon.spy()
+        };
+
+        await deleteTeam(req,res);
+
+        expect(findByIdStub.calledOnceWith(req.params.id)).to.be.true;
+        expect(res.status.calledWith(404)).to.be.true;
+        expect(res.json.calledWith({message:"Team Not Found"})).to.be.true;
+
+        findByIdStub.restore();
+    });
+
+    it("should return 500 if an error occurs", async() =>{
+        const findByIdStub = sinon.stub(Team, 'findById').throws(new Error("DB Error"));
+        const req = {params: {id: new mongoose.Types.ObjectId().toString()}};
+        const res={
+            status: sinon.stub().returnsThis(),
+            json: sinon.spy()
+        };
+
+        await deleteTeam(req,res);
+
+        expect(res.status.calledWith(500)).to.be.true;
+        expect(res.json.calledWithMatch({message: "DB Error"})).to.be.true;
+
+        findByIdStub.restore();
     });
 });
